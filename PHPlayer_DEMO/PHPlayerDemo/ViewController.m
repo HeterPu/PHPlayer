e75a90600bc8bc197f49bf4ad41694e7cbc0dafb
Peter <wycgpeterhu@sina.com>
2018-02-28 10:44:29 +0800

    fix bugs

--- PHPlayer_DEMO/PHPlayerDemo/ViewController.m
+++ PHPlayer_DEMO/PHPlayerDemo/ViewController.m
@@ -22,9 +22,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-  
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:@"dismiss" forState:UIControlStateNormal];
+    btn.frame = CGRectMake(20, 20, 60, 40);
+    [btn addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+-(void)click:(id)sender{
+     [self dismissViewControllerAnimated:true completion:nil];
+}
+
 
 
 
@@ -46,7 +59,7 @@
     
     NSString *PATH = [[NSBundle mainBundle] pathForResource:@"miaobiao_h.MOV" ofType:nil];
     NSURL *urll = [NSURL fileURLWithPath:PATH];
-    _player1 = [PHPlayer playerWithFrame:CGRectMake(0, 50, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
+    _player1 = [PHPlayer playerWithFrame:CGRectMake(0, 100, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
     _player1.backgroundColor = [UIColor blackColor];
     
     [self.view addSubview:_player1];
@@ -59,7 +72,7 @@
     
     NSString *PATH = [[NSBundle mainBundle] pathForResource:@"miaobiao_h.MOV" ofType:nil];
     NSURL *urll = [NSURL fileURLWithPath:PATH];
-    _player2 = [PH_Control_Player playerWithFrame:CGRectMake(0, 50, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
+    _player2 = [PH_Control_Player playerWithFrame:CGRectMake(0, 100, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
     _player2.backgroundColor = [UIColor blackColor];
     _player2.startPlayTime = @(3);
     _player2.endPlayTime = @(10);
@@ -74,7 +87,7 @@
     
     NSString *PATH = [[NSBundle mainBundle] pathForResource:@"miaobiao_h.MOV" ofType:nil];
     NSURL *urll = [NSURL fileURLWithPath:PATH];
-    _player3 = [PH_Control_Custom_Player playerWithFrame:CGRectMake(0, 50, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
+    _player3 = [PH_Control_Custom_Player playerWithFrame:CGRectMake(0, 100, 350, 280) contentOfUrl:urll.absoluteString playOptions:options];
     _player3.backgroundColor = [UIColor blackColor];
     _player3.startPlayTime = @(3);
     _player3.endPlayTime = @(10);
@@ -83,6 +96,22 @@
 }
 
 
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    if (_index == 1) {
+        [_player1 destoryPlayer];
+    }else if (_index == 2){
+        [_player2 destoryPlayer];
+    }else{
+        [_player3 destoryPlayer];
+    }
+}
+
+
+- (void)dealloc
+{
+    NSLog(@"Freeeeee");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
